a9863fec539e57fd2ccf910c0b22dc5cabcd89ac
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2026-06-12 16:50:12 -0300

    Update Analisador.java

--- Algoritmos e Programação/Trabalho Final/Analisador/src/Analisador.java
+++ Algoritmos e Programação/Trabalho Final/Analisador/src/Analisador.java
@@ -4,7 +4,7 @@ public class Analisador {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
 
-        int MAXIMO_ELEMENTOS = 10 ;
+        int MAXIMO_ELEMENTOS = 3;
         int[] numeros = new int[MAXIMO_ELEMENTOS];
         int totalNumeros = 0;
 
@@ -54,7 +54,80 @@ public class Analisador {
             if (numeros[i] < menor) menor = numeros[i];
         }
         System.out.println("]");
-        System.out.println("Quantidade total de númeroa informados: " + totalNumeros);
+        System.out.println("Quantidade total de números informados: " + totalNumeros);
+
+        double media = soma / totalNumeros;
+        System.out.printf("Média Aritmética dos números: %.2f\n", media);
+        System.out.println("Maior número da lista: " + maior);
+        System.out.println("Menor número da lista: " + menor);
+
+        int totalPrimos = 0;
+        int menorPrimo = -1;
+        int maiorPrimo = -1;
+
+        for (int i = 0; i < totalNumeros; i++) {
+            if (ePrimo(numeros[i])) {
+                totalPrimos++;
+                if (maiorPrimo == -1 || numeros[i] > maiorPrimo) {
+                    maiorPrimo = numeros[i];
+                }
+                if (menorPrimo == -1 || numeros[i] < menorPrimo) {
+                    menorPrimo = numeros[i];
+                }
+            }
+        }
+
+        System.out.println("\n--- Análise de Primos ---");
+        if (totalPrimos > 0) {
+            System.out.println("Quantidade de números primos encontrados: " + totalPrimos);
+            System.out.println("Maior número primo da lista: " + maiorPrimo);
+            System.out.println("Menor número primo da lista: " + menorPrimo);
+        } else {
+            System.out.println("Nenhum número primo encontrado");
+        }
+
+        int maxFrequencia = 0;
+        for (int i = 0; i < totalNumeros; i++) {
+            int contagem = 0;
+            for (int j = 0; j < totalNumeros; j++) {
+                if (numeros[i] == numeros[j]) {
+                    contagem++;
+                }
+            }
+            if (contagem > maxFrequencia) {
+                maxFrequencia = contagem;
+            }
+        }
+
+        System.out.println("\n--- Análise de Frequência (Moda) ---");
+        System.out.print("Número(s) mais repetido(s): ");
+
+        int[] modasImpressas = new int[totalNumeros];
+        int qtdModasImpressas = 0;
+
+        for (int i = 0; i < totalNumeros; i++){
+            int contagem = 0;
+            for (int j = 0; j < totalNumeros; j++){
+                if (numeros[i] == numeros[j]) contagem++;
+            }
+
+            if (contagem == maxFrequencia){
+                boolean jaImpresso = false;
+                for (int k = 0; k < qtdModasImpressas; k++){
+                    if (modasImpressas[k] == numeros[i]){
+                        jaImpresso = true;
+                        break;
+                    }
+                }
+                if (!jaImpresso){
+                    System.out.print(numeros[i] + " ");
+                    modasImpressas[qtdModasImpressas] = numeros[i];
+                    qtdModasImpressas++;
+                }
+            }
+        }
+        System.out.println("\nQuantidade de aparições: " + maxFrequencia);
+        
     }
 
     private static void analisarNumeroIndividual(int num) {
@@ -87,7 +160,7 @@ public class Analisador {
 
     private static long calcularFatorial(int num) {
         long fat = 1;
-        for(int i = 1; i <= num; i++) {
+        for (int i = 1; i <= num; i++) {
             fat *= i;
         }
         return fat;
