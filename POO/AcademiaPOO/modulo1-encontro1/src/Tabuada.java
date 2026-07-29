d500c9b5b172e23b236a35832fba66f863e55cc1
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2026-07-29 19:54:55 -0300

    tabuada

--- POO/AcademiaPOO/modulo1-encontro1/src/Tabuada.java
+++ POO/AcademiaPOO/modulo1-encontro1/src/Tabuada.java
@@ -1,2 +1,17 @@
 public class Tabuada {
+
+    public static void imprimirTabuada(int numero) {
+        System.out.println("=== Tabuada do " + numero + " ===");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d × %d = %d%n", numero, i, numero * i);
+        }
+    }
+
+    public static void main(String[] args) {
+        imprimirTabuada(2);
+        System.out.println();
+        imprimirTabuada(5);
+        System.out.println();
+        imprimirTabuada(7);
+    }
 }
